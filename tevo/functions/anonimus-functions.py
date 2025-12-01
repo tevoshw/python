219ce438d1a9630e8e71bd3bf642b82d1b83e059
tevoshw <tevoshw@gmail.com>
2025-12-01 13:23:44 -0300

    anonymus function

---
+++ tevo/functions/anonimus-functions.py
@@ -0,0 +1,12 @@
+# IN PYTHON TO USE THE ANONYMUS FUNCTION WE NEED TO USE THE LAMBDA
+
+
+
+# FIRST EXAMPLE
+a = lambda num: num + 1
+print(a(5))
+
+# SECOND EXAMPLE
+
+a = lambda lista: [item + 1 for item in lista]
+print(a([1,2,3]))   
