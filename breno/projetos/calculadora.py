058791b6fc02506cebebc1666592521692775b31
brenotocchio <breno2007tocchio@gmail.com>
2025-12-02 17:36:56 -0300

    calculadora simples

---
+++ breno/projetos/calculadora.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+print(f"O resultado da soma dos dois digitos é {n1 + n2}.")
+print(f"O resultado da subtração dos dois digitos é {n1 - n2}.")
+print(f"O resultado da multiplicação dos dois digitos é {n1 * n2}.")
+print(f"O resultado da divisão dos dois digitos é {n1 / n2}.")
