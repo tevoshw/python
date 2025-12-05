33939275c4276da4ea940d5e5b198a283c74ddd9
tevoshw <tevoshw@gmail.com>
2025-12-05 16:47:44 -0300

    add anotations in python

---
+++ tevo/basics/anotations/anotations.py
@@ -0,0 +1,15 @@
+# X: A
+#  ORGANIZE E SECURITY OF THE CODE, BUT IT'S NOT NECESSARY IN SOME CASES
+
+
+numero_inteiro: int = 10
+nome: str =  "tevo"
+print(numero_inteiro, nome)
+
+
+
+def test_numbers(base: float,  height: float) -> float:
+    return base * height
+
+result: float =  test_numbers(1,2) 
+print(result)
