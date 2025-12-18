ee7e6e8464637e9aac650e2963d7881c1554883c
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-18 16:26:35 -0300

    primeiro dia de aprendizado

---
+++ jao/controle de estruturas/for.py
@@ -0,0 +1,4 @@
+dia = 0
+for n in range (10):
+    dia = dia + 1
+    print(dia)
---
+++ jao/controle de estruturas/if.py
@@ -0,0 +1,18 @@
+#exemplo 1
+idade = 20
+dia = 18
+if idade < dia:
+    print("Certo")
+else:
+    print("errado")
+
+#exemplo 2
+pista = False
+if pista:
+    print("acesso negado")
+
+#exemplo 3
+senha = "123"
+if senha == "1234":
+    print("liberado")
+
