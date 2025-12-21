57dd65075fe2bdde34c1e843a72bc9aa10469b91
tevoshw <tevoshw@gmail.com>
2025-12-21 00:09:16 -0300

    Add open and open .txt and .json // Read .txt and .json // Write .txt

---
+++ tevo/manage data/ler_arquivos_geral.py
@@ -0,0 +1,18 @@
+
+# TXT
+with open("tevo/manage data/main.txt") as txt:
+    print(txt.read())
+
+### OR
+
+txt = open("tevo/manage data/main.txt")
+print(txt.read())
+
+
+# .JSON
+with open("tevo/manage data/main.json") as json:
+    print(json.read())
+
+
+json = open("tevo/manage data/main.json")
+print(json.read())
---
+++ tevo/manage data/ler_arquivos_linha.py
@@ -0,0 +1,18 @@
+
+# TXT
+with open("tevo/manage data/main.txt") as txt:
+    print(txt.readline()) # WITH ITS A CICLE, START SOMETHING AND CLOSE AFTER, INDEPENDECE THE ACTIONS OF THE USER
+
+### OR
+
+txt = open("tevo/manage data/main.txt")
+print(txt.readline())
+
+
+# .JSON
+with open("tevo/manage data/main.json") as json:
+    print(json.readline())
+
+
+json = open("tevo/manage data/main.json")
+print(json.readline())
