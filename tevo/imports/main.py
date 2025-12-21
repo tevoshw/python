edd7cf2a26be6f659bed31d04ccb4b7df0ce6f23
tevoshw <tevoshw@gmail.com>
2025-12-20 23:03:32 -0300

    add import pages in python

---
+++ tevo/imports/main.py
@@ -0,0 +1,14 @@
+# THIS FOLDER ITS A WAY TO UNDERSTAND AND TRAINING ABOUT IMPORTS IN PYTHON
+
+class Animal:
+    def som(self):
+        print("O animal está produzindo som")
+    
+    def comer(self):
+        print("O animal está comendo")
+
+    def correr(self):
+        print("O animal está correndo")
+
+
+animal = Animal()
