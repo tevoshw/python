92702c010dbb404304abf65338df3e4d38a129c5
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-23 00:05:22 -0300

    projeto simples de verificador de senha usando def

--- jao/projects/password_checker.py
+++ jao/projects/password_checker.py
@@ -7,9 +7,11 @@ def password_checker():
     password = input("""Create you new password to login: 
 """)
 
+    #if not is verifying in the input if theres an number by the def no_number(password)
     if not no_number(password):
             print("Invalid! Your password MUST contain numbers.")
 
+    #elif not is verifying in the input if theres an letter by the def no_letter(password)
     elif not no_letter(password):
         print("Invalid! Your password MUST contain letters.")
     
@@ -20,16 +22,19 @@ def password_checker():
 
 def no_number(password):
     for char in password:
+        #verifying in every character of the input password if theres an digit, if not return the message, if yes ignores
         if char.isdigit():
             return True
     return False
 
 def no_letter(password):
     for char in password:
+        #verifying in every character of the input password if theres an letter, if not return the message, if yes ignores
         if char.isalpha():
             return True
     return False
 
+#this is an loop, while the password dont match with the requirements the code will run
 while True:
     if password_checker():
         break
