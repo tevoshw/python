8ec0df7935c9a101c5bfd7fde294414a4b9b7961
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-22 21:02:18 -0300

    verificador de senhas simples usando def

--- jao/projects/password_checker.py
+++ jao/projects/password_checker.py
@@ -1,28 +1,35 @@
+
 def password_checker():
-    print("Hi User! Welcome to our site. ")
+    print("""Hi User! Welcome to our site.
+          
+""")
     #the code will know if theres an letter or a number, so its not necessesary to put int()
-    password = input("Create you new password to login: ")
-    if not password.isdigit():
-        no_number()
-    elif not password.isalpha():
-        no_char()
+    password = input("""Create you new password to login: 
+""")
+
+    if not no_number(password):
+            print("Invalid! Your password MUST contain numbers.")
+
+    elif not no_letter(password):
+        print("Invalid! Your password MUST contain letters.")
+    
     else:
-        congratulations()
+        print("Congratulations! Your password was successfully created.")
+        return True
     
 
+def no_number(password):
+    for char in password:
+        if char.isdigit():
+            return True
+    return False
+
+def no_letter(password):
+    for char in password:
+        if char.isalpha():
+            return True
+    return False
 
 while True:
-
-    def no_number():
-        print("Invalid password! Your password must contain numbers.")
-
-
-    def no_char():
-        print("Invalid password! Your password must contain letters.")
-
-
-    def congratulations():
-        print("Congratulations! You password were successfully crated.")
-
-
-    password_checker()
+    if password_checker():
+        break
