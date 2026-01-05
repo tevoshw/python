aaca9c4cf675be196fa9da0cf9fcce061b6f8fb8
JvcCoding <joaovitorcordeiro2008@gmail.com>
2026-01-05 15:16:10 -0300

    Aprendendo classes

---
+++ jao/poo/classes.py
@@ -0,0 +1,21 @@
+
+
+
+class Teste():
+    def __init__(self):
+        self.meta = 100
+        self.vendas = 0
+
+    def venda(self, vendas):
+        self.vendas = vendas
+        print(f"Voce fez um total de {self.vendas}")
+        self.bateu_meta()
+
+    def bateu_meta(self):
+        if self.vendas > self.meta:
+            print("Voce bateu a meta")
+        else:
+            print("Voce nao bateu a meta")
+
+x = Teste()
+x.venda(50)
