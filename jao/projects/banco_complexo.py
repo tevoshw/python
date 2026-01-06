e42054695c56ef5724d9b2abebc157dd64c4f334
JvcCoding <joaovitorcordeiro2008@gmail.com>
2026-01-06 17:13:10 -0300

    Programa de banco usando class, def e lista

--- jao/projects/banco_complexo.py
+++ jao/projects/banco_complexo.py
@@ -1,4 +1,4 @@
-class banco():
+class banco:
   def __init__(self):
     self.saldo = 0
     self.ver_historico = []
@@ -13,7 +13,6 @@ class banco():
 3. Consultar saldo
 4. Ver historico de transações
 5. Desligar
-                     
 """))
       if opcs == 1:
         qtd_saq = int(input("""Qual quantia o senhor deseja sacar?
@@ -22,9 +21,11 @@ class banco():
           print("""Você não pode sacar uma quantia maior do que tem na conta!
                 """)
         else:
-          self.saldo = qtd_saq - self.saldo
-          self.ver_historico.append(f"Saque de {qtd_saq} realizado com sucesso!")
-      
+          self.saldo = self.saldo - qtd_saq
+          self.ver_historico.append(f"Saque de {qtd_saq}R$ realizado com sucesso!")
+          print(f"""Saque de {qtd_saq} realizado com sucesso!
+""")
+
       elif opcs == 2:
         qtd_dep = int(input("""Qual quantia o senhor deseja depositar?
 """))
@@ -33,11 +34,21 @@ class banco():
                 """)
         else:
           self.saldo = qtd_dep + self.saldo
-          self.ver_historico.append(f"Deposito de {qtd_dep} realizado com sucesso!")
-          print(f"Deposito de {qtd_dep} realizado com sucesso!")
+          self.ver_historico.append(f"Deposito de {qtd_dep}R$ realizado com sucesso!")
+          print(f"""Deposito de {qtd_dep} realizado com sucesso!
+""")
+      elif opcs == 3:
+        print(f"O seu saldo é de {self.saldo}R$")
+
+      elif opcs == 4:
+        for item in self.ver_historico:
+          print(item)
+      
+      elif opcs == 5:
+        print("Obrigado por usar o caixa complexo!")
+        break
 
 
 
 start = banco()
 start.escolha()
-
