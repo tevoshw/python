608a84215b803349c228f4acae6b787bf50e39a6
tevoshw <tevoshw@gmail.com>
2026-01-08 14:49:43 -0300

    add PART1 PROJECT1

---
+++ tevo/projects/banco.py
@@ -0,0 +1,25 @@
+class Banco():
+    def __init__(self):
+        self.dinheiro = 1000
+
+    def sacar(self):
+        quantidade_sacar = int(input("Digite a quantidade de dinheiro que vocẽ deseja sacar: "))
+        if quantidade_sacar > self.dinheiro:
+            print("Vocẽ não possui esse dinheiro suficiente na sua conta!")
+        else:
+            self.dinheiro -= quantidade_sacar
+
+    def ver_dinheiro(self):
+        print(f"O dinheiro disponível na sua conta é de: {self.dinheiro}")
+    
+    def depositar(self):
+        quantidade_depositar = int(input("Digite a quantidade que você deseja depositar"))
+        if quantidade_depositar <= 0:
+            print("Você não pode depositar esse valor, por ser 0 ou menor que ele!")
+        else:
+            self.dinheiro += quantidade_depositar
+
+
+
+
+
