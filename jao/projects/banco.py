e34baa68a9fdf153fd843b03dde0c8b5b998f8c0
tevoshw <tevoshw@gmail.com>
2026-01-08 15:08:58 -0300

    add PRINT to the PROJECT1

--- jao/projects/banco.py
+++ jao/projects/banco.py
@@ -8,16 +8,19 @@ class Banco():
             print("Vocẽ não possui esse dinheiro suficiente na sua conta!")
         else:
             self.dinheiro -= quantidade_sacar
+            print(f"Seu dinheiro agora é de: {self.dinheiro}")
+
 
     def ver_dinheiro(self):
         print(f"O dinheiro disponível na sua conta é de: {self.dinheiro}")
     
     def depositar(self):
-        quantidade_depositar = int(input("Digite a quantidade que você deseja depositar"))
+        quantidade_depositar = int(input("Digite a quantidade que você deseja depositar: "))
         if quantidade_depositar <= 0:
             print("Você não pode depositar esse valor, por ser 0 ou menor que ele!")
         else:
             self.dinheiro += quantidade_depositar
+            print(f"Seu dinheiro agora é de: {self.dinheiro}")
 
 
 
