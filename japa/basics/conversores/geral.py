e638f360c0de64c08f38e3f9564df2c9cd723f0e
japaafk <matheushirose4@gmail.com>
2026-01-22 15:18:43 -0300

    apredendo as coisas básicas de python

---
+++ japa/basics/conversores/geral.py
@@ -0,0 +1,13 @@
+#inteiro para float
+n_inteiro = 10
+conversor = float(n_inteiro)
+print(f'numero antes da conversão {n_inteiro}, numero depois da conversão {conversor}')
+
+#float para inteiro
+n_float = 3.14
+conversor1 = int(n_float)
+print(f'numero antes da conversao {n_float}, numero depois da conversão {conversor1}')
+
+#inteiro para string
+conversor2 = str(n_inteiro)
+print(f"antes da conversão {n_inteiro}, depois da conversão {conversor2}")
---
+++ japa/basics/input/input.py
@@ -0,0 +1,9 @@
+#nulo = 0
+#print(f'o valor da variável antes do input é {nulo}')
+
+#nulo = input('digite um novo valor: ')
+#print(f'agora o valor pós input é {nulo}')
+numero1 = int(input('digite o primeiro numero: '))
+numero2 = int(input('digite o segundo numero: '))
+resultado = numero1 + numero2
+print(resultado)
