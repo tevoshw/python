4a44cd5b186c15f9d582e00cc8645d933e4d6060
japaafk <matheushirose4@gmail.com>
2026-01-23 15:27:24 -0300

    estudando a estrutura for

---
+++ japa/estrutura de controle/for.py
@@ -0,0 +1,4 @@
+primeiro_termo = int(input('Digite o primeiro termo: '))
+ultimo_termo = int(input('Digite o ultimo termo: '))
+for numero in range(primeiro_termo, ultimo_termo+1):
+    print(numero)
---
+++ japa/estrutura de controle/if_e_else.py
@@ -0,0 +1,8 @@
+vari1 = int(input('Digite um número: '))
+vari2 = int(input('Digite um outro número: '))
+if vari1 > vari2:
+    print('o primeiro número é maior')
+elif vari1 == vari2:
+    print('eles são iguais')
+else:
+    print('o segundo número é maior')
