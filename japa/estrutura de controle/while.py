2a822a30b407b5f859911ca3f80e8c4a75d00ca8
japaafk <matheushirose4@gmail.com>
2026-01-23 16:30:59 -0300

    sintaxe do comando while

---
+++ japa/estrutura de controle/while.py
@@ -0,0 +1,5 @@
+valor_inicial = int(input('digite um valor inicial: '))
+valor_final = int(input('digite um valor final: '))
+while valor_inicial <= valor_final:
+    print(valor_inicial)
+    valor_inicial += 1
