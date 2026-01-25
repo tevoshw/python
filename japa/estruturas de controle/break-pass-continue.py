18562de15f9c0e6d5dc2d7f84bfd0504d29e7989
japaafk <matheushirose4@gmail.com>
2026-01-25 16:18:09 -0300

    estruturas de controle para loop

---
+++ japa/estruturas de controle/break-pass-continue.py
@@ -0,0 +1,18 @@
+numero1 = int
+for numero1 in range(0, 25):
+    pass
+
+#numero2 = int(input('Digite um valor inicial: '))
+#numero3 = int(input('Digite um valor final: '))
+#if numero2 < numero3:
+    #for sequencia in range(numero2, numero3+1):
+        #if sequencia % 2 == 0:
+            #continue
+        #print(sequencia)
+
+
+
+
+for numeros in range(21):
+    if numeros == 21:
+        break
