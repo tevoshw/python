a1c2df95b69f348f9e6f40dd4610144457419ff0
japaafk <matheushirose4@gmail.com>
2026-01-29 13:43:04 -0300

    sintaxe da estrutura match case

---
+++ japa/estruturas de controle/match_case.py
@@ -0,0 +1,22 @@
+dia = 265
+
+match dia:
+    case 1:
+        print('domingo')
+    case 2:
+        print('segunda')
+    case 3:
+        print('terça')
+    case 4:
+        print('quarta')
+    case 5:
+        print('quinta')
+    case 6:
+        print('sexta')
+    case 7:
+        print('sábado')
+
+    # caso não seje executado nenhum dos casos acima
+    # pode ser colocado também uma outra variável para receber a variável dia, mas não tem necessidade
+    case _:
+        print('dia inválido')
---
+++ japa/mini projetos/calculadora.py
@@ -0,0 +1,11 @@
+print('Olá bem vindo a sua calculadora digital')
+calculo = ''
+while calculo != "":
+    calculo = input('''Que tipo de cálculo você gostaria?
+                    1- adição
+                    2- subtração
+                    3- multiplicação
+                    4- divisão
+                    5- potenciação
+                    6- radiciação
+                    Resposta: ''')
